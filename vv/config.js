9d31971b9f68dc4e0c34fddc9a7ff52789d0c6a9
CodHeK <gaganganapathyas@yahoo.in>
2019-01-26 16:33:16 +0530

    parser added, not working :(

---
+++ vv/config.js
@@ -0,0 +1,14 @@
+exports.root = {
+  div: {
+    id: 'root',
+    nav : {
+      class: 'navbar',
+    },
+    div : {
+      h1: {
+        class: 'title',
+        value: 'PG<3NS'
+      }
+    }
+  }
+}
